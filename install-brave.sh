d5c1cd2673e9a99ae8d04325bea44122f606a414
Nicholas <nicholas.osprey@outlook.com>
2021-12-11 00:57:03 -0800

    Launch

---
+++ install-brave.sh
@@ -0,0 +1,40 @@
+# INSTALL OPTIONS
+brave_stable() {
+	sudo apt install apt-transport-https curl
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+	sudo apt update
+	sudo apt install brave-browser
+}
+
+brave_nightly() {
+	sudo apt install apt-transport-https curl
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg arch=amd64] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list
+	sudo apt update
+	sudo apt install brave-browser-nightly
+}
+
+brave_beta() {
+	sudo apt install apt-transport-https curl
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-beta-archive-keyring.gpg https://brave-browser-apt-beta.s3.brave.com/brave-browser-beta-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+	sudo apt update
+	sudo apt install brave-browser-beta
+}
+
+# MAIN
+echo "Which do you want? 1] Release 2) Beta 3) Nightly"
+read $choice
+
+case $choice in
+	("2")
+		brave_beta
+		;;
+	("3")
+		brave_nightly
+		;;
+	("1" | *)
+		brave_stable
+		;;
+esac
